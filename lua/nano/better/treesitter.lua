0f439f042a2db1d9dde0865fcc73b635e7b40d30
Gaël Philippe <gaelph@gmail.com>
2024-05-14 19:39:22 +0200

    refactor: organise treesitter file

--- lua/nano/better/treesitter.lua
+++ lua/nano/better/treesitter.lua
@@ -2,34 +2,45 @@ return function(variant)
 	local faces = require "nano.better.palette"(variant).faces
 
 	return {
+		-- BASE
 		["@attribute"] = {
 			fg = faces.faded.fg,
 			ctermfg = faces.faded.ctermfg,
 			nocombine = true,
 		},
-		["@attribute.builtin.swift"] = { link = "Salient" },
-		["@attribute.css"] = { link = "Normal" },
-		["@comment.documentation.javascript"] = { link = "Faded" },
-		["@comment.documentation.lua"] = { link = "Faded" },
 		["@function"] = {
 			fg = faces.normal.fg,
 			ctermfg = faces.normal.ctermfg,
 			bold = true,
 			nocombine = true,
 		},
-		["@keyword.jsdoc"] = { link = "Salient" },
-		["@lsp.mod.documentation.lua"] = { link = "Salient" },
+		["@property"] = { link = "Default" },
 		["@lsp.type.comment"] = { link = "Comment" },
+		-- MARK: - Languages -----------------------------------------------------
+		-- CSS
+		["@attribute.css"] = { link = "Normal" },
+		-- JAVASCRIPT
+		["@comment.documentation.javascript"] = { link = "Faded" },
+		["@keyword.jsdoc"] = { link = "Salient" },
+		-- LUA
+		["@comment.documentation.lua"] = { link = "Faded" },
+		["@lsp.mod.documentation.lua"] = { link = "Salient" },
 		["@lsp.type.type.lua"] = { link = "Salient" },
 		["@lsp.typemod.keyword.documentation.lua"] = { link = "Salient" },
-		["@property"] = { link = "Default" },
+		-- SWIFT
+		["@attribute.builtin.swift"] = { link = "Salient" },
+		-- MARK: - Non-Languages -------------------------------------------------
+		-- JSON
 		["@property.json"] = { link = "Salient" },
-		["@property.yaml"] = { link = "Salient" },
 		["@string.json"] = { link = "Default" },
+		-- YAML
+		["@property.yaml"] = { link = "Salient" },
 		["@string.yaml"] = { link = "Default" },
+		-- XML/HTML/JSX
 		["@tag"] = { link = "Bold" },
 		["@tag.attribute"] = { link = "Default" },
 		["@tag.delimiter"] = { link = "Faded" },
+		-- comment notes
 		["@text.danger"] = { link = "CriticalI" },
 		["@text.mark"] = { fg = faces.normal.fg, italic = true },
 		["@text.note"] = { link = "Bold" },
@@ -39,8 +50,10 @@ return function(variant)
 			ctermfg = faces.popout.ctermfg,
 			bold = true,
 		},
+		-- markup (markdown, org, vimdoc)
 		["@markup.heading"] = { link = "strong" },
 		["@markup.link"] = { fg = faces.salient.fg, underline = true },
+		["@string.special.url"] = { link = "@markup.link" },
 		["@punctuation.special"] = { link = "Comment" },
 		["@markup.quote"] = { italic = true },
 	}
