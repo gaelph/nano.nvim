c8fc5178fbbb9101846948bdfbb4090b01de3bb2
Gaël Philippe <gaelph@gmail.com>
2025-03-02 00:04:48 +0100

    fix term colors

--- lua/nano/utils.lua
+++ lua/nano/utils.lua
@@ -1,21 +1,8 @@
 local Utils = {}
 
 function Utils.auto_term_gui_colors()
-	local TERM = os.getenv "TERM"
-	local TERM_PROGRAM = os.getenv "TERM_PROGRAM"
 	local COLORTERM = os.getenv "COLORTERM"
-	if
-		COLORTERM ~= "truecolor"
-		and TERM ~= nil
-		and TERM ~= "xterm-kitty"
-		and vim.startswith(TERM, "xterm")
-		and TERM_PROGRAM ~= "WezTerm"
-		and TERM_PROGRAM ~= "ghostty"
-	then
-		vim.o.termguicolors = false
-	else
-		vim.o.termguicolors = true
-	end
+	vim.o.termguicolors = COLORTERM == "truecolor"
 end
 
 Utils.ns = vim.api.nvim_create_namespace "nano-ns"
