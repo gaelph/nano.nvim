a581328ef00f1fabcdea42772545ebc79ada87bb
Gaël Philippe <gaelph@gmail.com>
2025-03-02 00:05:29 +0100

    better looking diffs

--- lua/nano/better/diff.lua
+++ lua/nano/better/diff.lua
@@ -1,10 +1,14 @@
 return function(variant)
 	if variant == "light" then
 		return {
-			DiffAdd = { fg = "#43605E", bg = "#E6EAE6" },
-			DiffChange = { fg = "#3D4261", bg = "#DAD6E0" },
-			DiffDelete = { fg = "#5D5246", bg = "#E7DCD5" },
-			DiffText = { bg = "#DDD9E2" },
+			DiffAdd = { fg = "#245537", bg = "#dbecd4" },
+			-- DiffAdd = { fg = "#43605E", bg = "#E6EAE6" },
+			DiffChange = { fg = "#3D4261", bg = "#eff5f6" },
+			-- DiffChange = { fg = "#3D4261", bg = "#DAD6E0" },
+			DiffDelete = { fg = "#714642", bg = "#f4efef" },
+			-- DiffDelete = { fg = "#5D5246", bg = "#E7DCD5" },
+			DiffText = { bg = "#e4eaeb" },
+			DiffStrikeThrough = { fg = "#714642", strikethrough = true },
 			Added = { link = "Success" },
 			Changed = { link = "Salient" },
 			Removed = { link = "CriticalI" },
